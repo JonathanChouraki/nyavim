1005be228f72fc1b93e4dd2b585481a51000aff1
Jonathan Chouraki <jonathan.chouraki@gmail.com>
2022-03-10 15:28:20 +0100

    Added some keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -8,13 +8,28 @@ function executeCmd(cmd)
    return '<cmd> ' .. cmd .. ' <cr>'
 end
 
-vim.g.mapleader = ','
+vim.g.mapleader = ' '
 
+-- split
 key(normalMode, '<leader>s', executeCmd('vsplit'), opts)
 key(normalMode, '<leader>b', executeCmd('split'), opts)
+
+-- tab
 key(normalMode, '<leader>t', executeCmd('tabnew'), opts)
+
+-- highlight deactivation
 key(normalMode, '<leader><space>', executeCmd('noh'), opts)
+
+-- windows
 key(normalMode, '<leader>h', executeCmd('wincmd h'), opts)
 key(normalMode, '<leader>j', executeCmd('wincmd j'), opts)
 key(normalMode, '<leader>k', executeCmd('wincmd k'), opts)
 key(normalMode, '<leader>l', executeCmd('wincmd l'), opts)
+
+--jump to matching symbol
+key(normalMode, '<tab>', '%', opts)
+key(visualMode, '<tab>', '%', opts)
+
+-- make yanking behave like delete and change
+key(visualMode, 'Y', 'y$', opts)
+
