ec5fd52ad7eaf8b009d36c127ce471e469e2c144
Jonathan Chouraki <jonathan.chouraki@gmail.com>
2023-08-26 14:23:27 +0200

    Added jumplist to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,7 +8,7 @@ utils.normalModeKeybind('<leader>fb', 'lua require("telescope.builtin").buffers(
 utils.normalModeKeybind('<leader>fm', 'lua require("telescope.builtin").marks()')
 utils.normalModeKeybind('<leader>f*', 'lua require("telescope.builtin").grep_string()')
 utils.normalModeKeybind('<leader>f/', 'lua require("telescope.builtin").current_buffer_fuzzy_find()')
+utils.normalModeKeybind('<leader>fj', 'lua require("telescope.builtin").jumplist()')
 utils.normalModeKeybind('<leader>gb', 'lua require("telescope.builtin").git_branches()')
 utils.normalModeKeybind('<leader>gs', 'lua require("telescope.builtin").git_stash()')
 utils.normalModeKeybind('<leader>gc', 'lua require("telescope.builtin").git_commits()')
-
